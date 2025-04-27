f80ca3388b7b3c4edb374988c95b2c86314f6d78
elifhatuncolak <celifn89@gmail.com>
2025-04-27 16:18:15 +0300

    odev1kt.

---
+++ DegiskenOluşturma.kt
@@ -0,0 +1,161 @@
+package com.example.kotlindersleri.elifhatuncolakk
+ fun main() {
+    var sehir = "Bursa"
+    print("Şehir : $sehir")
+
+    var ulke = "İzlanda"
+    print("Ülke : $ulke")
+
+    var telefon = "05555555555"
+    print("Telefon : $telefon")
+
+
+    var postaKodu = "34000"
+     print("PostaKodu : $postaKodu")
+
+
+     var email = "celifn89@gmail.com"
+     print("Email : $email")
+
+     var meslek = "Ekonomist"
+     print("Meslek : $meslek")
+
+     var stokMiktari = "50000"
+     println("StokMiktarı : $stokMiktari")
+
+     var musteriAdi = "Elif Hanım"
+     println("MüşteriAdı : $musteriAdi")
+
+     var bakiye = "1000000000"
+     println("Bakiye : $bakiye")
+
+     var dogumGünü = "21 Kasım 2003 "
+     println("DoğumGünü : $dogumGünü")
+
+
+     var maas = "92000"
+     println("Maaş : $maas")
+
+
+     var medeniDurum = "Bekar"
+     println("Medeni Durum : $medeniDurum")
+
+     var urunYorumu = "Harika Bir Ürün"
+     println("Ürün Yorumu : $urunYorumu")
+
+     var odemeTarihi = "21 Nisan 2025"
+     println("Ödeme Tarihi : $odemeTarihi")
+
+     var odeme = "Tamamlandı"
+     println("Ödeme : $odeme")
+
+     var siparisAdedi = "5"
+     println("Sipariş Adedi : $siparisAdedi")
+
+     var arabaModeli = "Mercedes Benz SLR McLaren"
+     println("Araba Modeli : $arabaModeli")
+
+     var kitapAdi = "Bülbülü Öldürmek"
+     println("Kitap Adı : $kitapAdi")
+
+     var yayinlanmaTarihi = "30 Ocak 2005"
+     println("Yayınlanma Tarihi : $yayinlanmaTarihi")
+
+     var indirimMiktari = "%11"
+     println("İndirim Miktarı : $indirimMiktari")
+
+     var odaSayisi = "164"
+     println("Oda Sayısı : $odaSayisi")
+
+     var enlem = "Kuzey Enlemi= 39°57'49.89"
+     println("Enlem : $enlem")
+
+     var boylam = "Doğu Boylamı= 32°52'21.67'"
+     println("Boylam : $boylam")
+
+      var urunAdi = "Pilates Topu"
+     println("Ürün Adı  : $urunAdi")
+
+     var yemekFiyati = "218 TL"
+     println("Yemek Fiyatı : $yemekFiyati")
+
+     var marka = "McKinsey & Company"
+     println("Marka : $marka")
+
+     var muzikAdi = "Can Bonomo - Tastamam"
+     println("Müzik Adı : $muzikAdi")
+
+     var videoSuresi = "27:41"
+     println("Video Süresi : $videoSuresi")
+
+     var urunPuani = "5"
+     println("Ürün Puanı : $urunPuani")
+
+     var resimAdi = "resim.jpg"
+     println("Resim Adı : $resimAdi")
+
+     var dosyaFormati = "pdf"
+     println("Dosya Formatı : $dosyaFormati")
+
+     var renk = "yesil"
+     println("Renk : $renk")
+
+     var renkKodu = "00 FF 7F / 0 255 127"
+     println("Renk Kodu : $renkKodu")
+
+     var telefonModeli = "İphone 16"
+     println("Telefon Modeli : $telefonModeli")
+
+     var ekranBoyutu = "5.5 inç"
+     println("Ekran Boyutu : $ekranBoyutu")
+
+     var agirlik = "41 KG"
+     println("Ağırlık : $agirlik")
+
+     var ulusalGun = "19 Mayıs"
+     println("Ulusal Gün : $ulusalGun")
+
+     var tatilGunu = "Cumartesi"
+     print("Tatil Günü : $tatilGunu")
+
+     var  rezervasyonTarihi = "10 Eylül Perşembe"
+     println("Rezervasyon Tarihi : $rezervasyonTarihi")
+
+     var sokakAdi = "Manolya Sokak"
+     println("Sokak Adı : $sokakAdi")
+
+     var otobusHatti = "98S-1"
+     println("Otobüs Hattı : $otobusHatti")
+
+     var kalanDakika = "26:41"
+     println("Kalan Dakika : $kalanDakika")
+
+     var takipKodu = "27899641984"
+     println("Takip Kodu : $takipKodu")
+
+     var kuponSuresi = "5 Gün"
+     println("Kupon Süresi : $kuponSuresi")
+
+     var kuponKodu = "PANTOLON55"
+     println("Kupon Kodu : $kuponKodu")
+
+     var faturaAdresi = "Muğla , Türkiye"
+     println("Fatura Adresi : $faturaAdresi")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
